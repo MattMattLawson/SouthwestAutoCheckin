6925292420a8cb3d6007167822f28c173a77cb4e
MattMattLawson <MattMattLawson@users.noreply.github.com>
2020-06-08 07:55:13 -0600

    Installed and added Chrome driver

---
+++ SouthwestAutoCheckin.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('/Users/matt/Google Drive/SouthwestAutoCheckin/chromedriver')
+
+driver.get('https://www.southwest.com/air/check-in/index.html')
